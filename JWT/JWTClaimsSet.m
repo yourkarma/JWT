5dab5ff26263fde63722fa92ae4becac1a1d3ea8
Klaas Pieter Annema <klaaspieter@annema.me>
2013-05-31 14:19:53 +0200

    Add the claims set model

---
+++ JWT/JWTClaimsSet.m
@@ -0,0 +1,13 @@
+//
+//  JWTClaimsSet.m
+//  JWT
+//
+//  Created by Klaas Pieter Annema on 31-05-13.
+//  Copyright (c) 2013 Karma. All rights reserved.
+//
+
+#import "JWTClaimsSet.h"
+
+@implementation JWTClaimsSet
+
+@end
