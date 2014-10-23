d2b2e508ae425898ee6b8d8141b4ea884ff230d9
Hernan Zalazar <hernanzalazar@gmail.com>
2014-10-23 18:37:07 -0300

    Add RS256 algorithm

---
+++ JWT/JWTAlgorithmRS256.h
@@ -0,0 +1,14 @@
+//
+//  JWTAlgorithmRS256.h
+//  JWT
+//
+//  Created by Hernan Zalazar on 10/23/14.
+//  Copyright (c) 2014 Karma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JWTAlgorithm.h"
+
+@interface JWTAlgorithmRS256 : NSObject<JWTAlgorithm>
+
+@end
