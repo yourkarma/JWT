8e51cdaa52c7091acb85b3772d8ca6a1574c2443
Lobanov Dmitry <gaussblurinc@gmail.com>
2016-02-03 20:48:27 +0300

    jwt: algorithm rs256 mock added

---
+++ JWTTests/JWTAlgorithmRS256Spec.m
@@ -0,0 +1,33 @@
+//
+//  JWTAlgorithmRS256Spec.m
+//  JWT
+//
+//  Created by Lobanov Dmitry on 21.11.15.
+//  Copyright © 2015 Karma. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import <Base64/MF_Base64Additions.h>
+
+#import "JWTAlgorithmRS256.h"
+
+
+SPEC_BEGIN(JWTAlgorithmRS256Spec)
+
+__block JWTAlgorithmRS256
+*algorithm;
+
+beforeEach(^{
+  algorithm = [[JWTAlgorithmRS256 alloc] init];
+});
+
+it(@"name is RS256", ^{
+  [[algorithm.name should] equal:@"RS256"];
+});
+
+it(@"HMAC encodes the payload using RSA256", ^{
+  NSData *encodedPayload = [algorithm encodePayload:@"payload" withSecret:@"secret"];
+    [[[encodedPayload base64String] should] equal:@"payload"];//@"uC/LeRrOxXhZuYm0MKgmSIzi5Hn9+SMmvQoug3WkK6Q="];
+});
+
+SPEC_END
