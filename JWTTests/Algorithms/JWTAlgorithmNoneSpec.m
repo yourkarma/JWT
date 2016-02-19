32d98f7edcff1daaad82ef15333828a878cedb47
John Brophy <jbrophy@uber.com>
2016-02-19 15:05:57 -0800

    updated algorithm unit tests to test new verification logic

--- JWTTests/Algorithms/JWTAlgorithmNoneSpec.m
+++ JWTTests/Algorithms/JWTAlgorithmNoneSpec.m
@@ -8,6 +8,7 @@
 
 #import <Kiwi/Kiwi.h>
 #import <Base64/MF_Base64Additions.h>
+#import "NSData+JWT.h"
 
 #import "JWTAlgorithmNone.h"
 
@@ -26,6 +27,30 @@ it(@"name is none", ^{
 it(@"should not encode payload and return emptry signature instead", ^{
     NSData *encodedPayload = [algorithm encodePayload:@"payload" withSecret:@"secret"];
     [[[encodedPayload base64Encoding] should] equal:@""];
+});
+
+it(@"should not verify JWT with a secret provided", ^{
+    NSString *secret = @"secret";
+    NSString *signingInput = @"eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
+    NSString *signature = nil;
+    
+    [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKey:secret]) should] beFalse];
+});
+
+it(@"should not verify JWT with a signature provided", ^{
+    NSString *secret = nil;
+    NSString *signingInput = @"eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
+    NSString *signature = @"signed";
+    
+    [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKey:secret]) should] beFalse];
+});
+
+it(@"should verify JWT with no signature and no secret provided", ^{
+    NSString *secret = nil;
+    NSString *signingInput = @"eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
+    NSString *signature = nil;
+    
+    [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKey:secret]) should] beTrue];
 });
 
 SPEC_END
