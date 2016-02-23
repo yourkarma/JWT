b2b9d794d03363cc6e10eb03c233c449e11f98f4
John Brophy <jbrophy@uber.com>
2016-02-22 19:06:32 -0800

    added basic headerdoc comments to JWTBuilder properties

--- JWT/JWT.h
+++ JWT/JWT.h
@@ -143,28 +143,113 @@ typedef NS_ENUM(NSInteger, JWTError) {
 + (JWTBuilder *)encodeClaimsSet:(JWTClaimsSet *)claimsSet;
 + (JWTBuilder *)decodeMessage:(NSString *)message;
 
+/**
+ The JWT in it's encoded form. Will be decoded and verified
+ */
 @property (copy, nonatomic, readonly) NSString *jwtMessage;
+
+/**
+ The payload dictionary to encode
+ */
 @property (copy, nonatomic, readonly) NSDictionary *jwtPayload;
+
+/**
+ The header dictionary to encode
+ */
 @property (copy, nonatomic, readonly) NSDictionary *jwtHeaders;
+
+/**
+ The expected JWTClaimsSet to compare against a decoded JWT
+ */
 @property (copy, nonatomic, readonly) JWTClaimsSet *jwtClaimsSet;
+
+/**
+ The verification key to use when encoding/decoding a JWT
+ */
 @property (copy, nonatomic, readonly) NSString *jwtSecret;
+
+/**
+ Contains the error that occured during an operation, or nil if no error occured
+ */
 @property (copy, nonatomic, readonly) NSError *jwtError;
+
+/**
+ The <JWTAlgorithm> to use for encoding a JWT
+ */
 @property (strong, nonatomic, readonly) id<JWTAlgorithm> jwtAlgorithm;
+
+/**
+ The algorithm name to use for decoding the JWT. Required unless force decode is true
+ */
 @property (copy, nonatomic, readonly) NSString *jwtAlgorithmName;
+
+/**
+ The force decode option. If set to true, a JWT won't be validated before decoding.
+ Should only be used for debugging
+ */
 @property (copy, nonatomic, readonly) NSNumber *jwtOptions;
+
+/*
+ Optional algorithm name whitelist. If non-null, a JWT can only be decoded using an algorithm
+ specified on this list.
+ */
 @property (copy, nonatomic, readonly) NSSet *algorithmWhitelist;
 
+/**
+ Sets jwtMessage and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^message)(NSString *message);
+
+/**
+ Sets jwtPayload and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^payload)(NSDictionary *payload);
+
+/**
+ Sets jwtHeaders and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^headers)(NSDictionary *headers);
+
+/**
+ Sets jwtClaimsSet and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^claimsSet)(JWTClaimsSet *claimsSet);
+
+/**
+ Sets jwtSecret and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^secret)(NSString *secret);
+
+/**
+ Sets jwtAlgorithm and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^algorithm)(id<JWTAlgorithm>algorithm);
+
+/**
+ Sets jwtAlgorithmName and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^algorithmName)(NSString *algorithmName);
+
+/**
+ Sets jwtOptions and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^options)(NSNumber *options);
+
+/**
+ Sets algorithmWhitelist and returns the JWTBuilder to allow for method chaining
+ */
 @property (copy, nonatomic, readonly) JWTBuilder *(^whitelist)(NSArray *whitelist);
 
+/**
+ Creates the encoded JWT string based on the currently set properties, or nil if an
+ error occured
+ */
 @property (copy, nonatomic, readonly) NSString *encode;
+
+/**
+ Decodes and returns the JWT as a dictionary, based on the JWTBuilder's currently set
+ properties, or nil, if an error occured.
+ */
 @property (copy, nonatomic, readonly) NSDictionary *decode;
 
 @end
