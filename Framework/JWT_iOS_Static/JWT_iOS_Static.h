1f0acd1de9da3c8d61941a0a7ef24ee9e16bd467
Lobanov Dmitry <gaussblurinc@gmail.com>
2016-08-12 19:19:44 +0300

    project: targets for supported platforms added.

---
+++ Framework/JWT_iOS_Static/JWT_iOS_Static.h
@@ -0,0 +1,13 @@
+//
+//  JWT_iOS_Static.h
+//  JWT_iOS_Static
+//
+//  Created by Lobanov Dmitry on 12.08.16.
+//  Copyright © 2016 Karma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JWT_iOS_Static : NSObject
+
+@end
---
+++ Framework/JWT_macOS_Framework/JWT_macOS_Framework.h
@@ -0,0 +1,19 @@
+//
+//  JWT_macOS_Framework.h
+//  JWT_macOS_Framework
+//
+//  Created by Lobanov Dmitry on 12.08.16.
+//  Copyright © 2016 Karma. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JWT_macOS_Framework.
+FOUNDATION_EXPORT double JWT_macOS_FrameworkVersionNumber;
+
+//! Project version string for JWT_macOS_Framework.
+FOUNDATION_EXPORT const unsigned char JWT_macOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWT_macOS_Framework/PublicHeader.h>
+
+
