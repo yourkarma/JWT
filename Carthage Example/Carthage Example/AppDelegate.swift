6dedbeb70812b017f63cfe0f76a9ee5a325bed50
Soheil <soheilbm@me.com>
2016-10-03 12:20:22 +0800

    minor update in public headers

--- Carthage Example/Carthage Example/AppDelegate.swift
+++ Carthage Example/Carthage Example/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        JWTBuilder.encodePayload(["hello" : "hello"])
+        
         return true
     }
 
--- JWT_Module/JWT_Module.h
+++ JWT_Module/JWT_Module.h
@@ -17,3 +17,18 @@ FOUNDATION_EXPORT const unsigned char JWT_ModuleVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <JWT_Module/PublicHeader.h>
 
 #import <JWT_Module/JWT.h>
+#import <JWT_Module/JWTAlgorithm.h>
+#import <JWT_Module/JWTAlgorithmDataHolder.h>
+#import <JWT_Module/JWTAlgorithmFactory.h>
+#import <JWT_Module/JWTAlgorithmHS256.h>
+#import <JWT_Module/JWTAlgorithmHS384.h>
+#import <JWT_Module/JWTAlgorithmHS512.h>
+#import <JWT_Module/JWTAlgorithmHSBase.h>
+#import <JWT_Module/JWTAlgorithmNone.h>
+#import <JWT_Module/JWTAlgorithmRSBase.h>
+#import <JWT_Module/JWTClaim.h>
+#import <JWT_Module/JWTClaimsSet.h>
+#import <JWT_Module/JWTClaimsSetSerializer.h>
+#import <JWT_Module/JWTClaimsSetVerifier.h>
+#import <JWT_Module/JWTDeprecations.h>
+#import <JWT_Module/JWTRSAlgorithm.h>
