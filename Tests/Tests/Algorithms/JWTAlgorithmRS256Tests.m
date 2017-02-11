b8703141232af3ea2d61c04faa8c6d92f7f8906b
Lobanov Dmitry <gaussblurinc@gmail.com>
2017-02-11 19:22:09 +0300

    jwt: rs tests updated.

--- Tests/Tests/Algorithms/JWTAlgorithmRS256Tests.m
+++ Tests/Tests/Algorithms/JWTAlgorithmRS256Tests.m
@@ -12,6 +12,7 @@
 #import "JWTAlgorithmRSBase.h"
 #import "JWTCryptoKeyExtractor.h"
 #import "JWTCryptoSecurity.h"
+#import "JWTCryptoKey.h"
 
 static NSString *algorithmBehavior = @"algorithmRS256Behaviour";
 static NSString *dataAlgorithmKey = @"dataAlgorithmKey";
@@ -177,6 +178,22 @@ sharedExamplesFor(algorithmBehavior, ^(NSDictionary *data) {
             }
             keyExtractingTokens = [mutableKeyExtractingTokens copy];
             keyExtractingDataHolders = [mutableKeyExtractingDataHolders copy];
+        });
+        context(@"Canonical", ^{
+            NSString *payloadString = @"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+            NSDictionary *payload = @{
+                @"sub": @"1234567890",
+                @"name": @"John Doe",
+                @"admin": @(YES)
+            };
+            NSString *signature = @"EkN-DOsnsuRjRO6BxXemmJDm3HbxrbRzXglbN2S4sOkopdU4IsDxTI8jO19W_A4K8ZPJijNLis4EZsHeY559a4DFOd50_OqgHGuERTqYZyuhtF39yxJPAjUESwxk2J5k_4zM3O-vtd1Ghyo4IbqKKSy6J9mTniYJPenn5-HIirE";
+            NSString *privateKeyPemEncodedString = @"MIICWwIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQABAoGAD+onAtVye4ic7VR7V50DF9bOnwRwNXrARcDhq9LWNRrRGElESYYTQ6EbatXS3MCyjjX2eMhu/aF5YhXBwkppwxg+EOmXeh+MzL7Zh284OuPbkglAaGhV9bb6/5CpuGb1esyPbYW+Ty2PC0GSZfIXkXs76jXAu9TOBvD0ybc2YlkCQQDywg2R/7t3Q2OE2+yo382CLJdrlSLVROWKwb4tb2PjhY4XAwV8d1vy0RenxTB+K5Mu57uVSTHtrMK0GAtFr833AkEA6avx20OHo61Yela/4k5kQDtjEf1N0LfI+BcWZtxsS3jDM3i1Hp0KSu5rsCPb8acJo5RO26gGVrfAsDcIXKC+bQJAZZ2XIpsitLyPpuiMOvBbzPavd4gY6Z8KWrfYzJoI/Q9FuBo6rKwl4BFoToD7WIUS+hpkagwWiz+6zLoX1dbOZwJACmH5fSSjAkLRi54PKJ8TFUeOP15h9sQzydI8zJU+upvDEKZsZc/UhT/SySDOxQ4G/523Y0sz/OZtSWcol/UMgQJALesy++GdvoIDLfJX5GBQpuFgFenRiRDabxrE9MNUZ2aPFaFp+DyAe+b4nDwuJaW2LURbr8AEZga7oQj0uYxcYw==";
+            NSString *publicKeyPemEncodedString = @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB";
+            NSString *algorithmName = @"RS256";
+            id<JWTRSAlgorithm> algorithm = (id<JWTRSAlgorithm>)[JWTAlgorithmFactory algorithmByName:algorithmName];
+            JWTCryptoKey *verifyKey = [[JWTCryptoKeyPublic alloc] initWithPemEncoded:publicKeyPemEncodedString parameters:nil error:nil];
+            algorithm.verifyKey = verifyKey;
+            [[theValue([algorithm verifySignedInput:payloadString withSignature:signature verificationKeyData:nil]) should] beTrue];
         });
         context(@"Decoding", ^{
             it(@"ByExtractedKeys", ^{
