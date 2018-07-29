bf83eebcda8263c9a06cc226c884e3e1bfb9fd69
Dmitry Lobanov <gaussblurinc@gmail.com>
2018-07-29 13:15:03 +0300

    tests: hs tests have been added.

--- Tests/Tests/Algorithms/JWTAlgorithmHS256Spec.m
+++ Tests/Tests/Algorithms/JWTAlgorithmHS256Spec.m
@@ -11,89 +11,70 @@
 
 #import <JWT/JWTAlgorithmHSBase.h>
 
-static NSString *dataAlgorithmKey = @"algorithm";
-static NSString *algorithmBehavior = @"algorithmHS256Behaviour";
+static __auto_type dataAlgorithmKey = @"algorithm";
+static __auto_type algorithmBehavior = @"algorithmHS256Behaviour";
 
 SHARED_EXAMPLES_BEGIN(JWTAlgorithmHS256SpecExamples)
 
 sharedExamplesFor(algorithmBehavior, ^(NSDictionary *data) {
     __block JWTAlgorithmHSBase *algorithm;
-    
+    __block __auto_type payload = @"payload";
+    __block __auto_type signedPayload = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+    __block __auto_type signedPayloadBase64 = @"uC/LeRrOxXhZuYm0MKgmSIzi5Hn9+SMmvQoug3WkK6Q=";
+    __block __auto_type secret = @"secret";
+    __block __auto_type wrongSecret = @"notTheSecret";
+    __block __auto_type signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
+    __block __auto_type name = @"HS256";
     beforeEach(^{
         algorithm = data[dataAlgorithmKey];
     });
     
     it(@"name is HS256", ^{
-        [[algorithm.name should] equal:@"HS256"];
+        [[algorithm.name should] equal:name];
     });
     
     it(@"HMAC encodes the payload canonically", ^{
-        NSString *payload = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *secret = @"secret";
-        NSString *signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
-        
-        [[theValue([algorithm verifySignedInput:payload withSignature:signature verificationKey:secret]) should] beTrue];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:signature verificationKey:secret]) should] beTrue];
     });
     
     it(@"HMAC encodes the payload using SHA256", ^{
-        NSData *encodedPayload = [algorithm encodePayload:@"payload" withSecret:@"secret"];
-        [[[encodedPayload base64String] should] equal:@"uC/LeRrOxXhZuYm0MKgmSIzi5Hn9+SMmvQoug3WkK6Q="];
+        __auto_type encodedPayload = [algorithm encodePayload:payload withSecret:secret];
+        [[[encodedPayload base64String] should] equal:signedPayloadBase64];
     });
     
     it(@"HMAC encodes the payload data using SHA256", ^{
-        NSData *payloadData = [NSData dataWithBase64String:[@"payload" base64String]];
-        NSData *secretData = [NSData dataWithBase64String:[@"secret" base64String]];
+        __auto_type payloadData = [NSData dataWithBase64String:[payload base64String]];
+        __auto_type secretData = [NSData dataWithBase64String:[secret base64String]];
         
-        NSData *encodedPayload = [algorithm encodePayloadData:payloadData withSecret:secretData];
-        [[[encodedPayload base64String] should] equal:@"uC/LeRrOxXhZuYm0MKgmSIzi5Hn9+SMmvQoug3WkK6Q="];
+        __auto_type encodedPayload = [algorithm encodePayloadData:payloadData withSecret:secretData];
+        [[[encodedPayload base64String] should] equal:signedPayloadBase64];
     });
     
     it(@"should verify JWT with valid signature and secret", ^{
-        NSString *secret = @"secret";
-        NSString *signingInput = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
-        
-        [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKey:secret]) should] beTrue];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:signature verificationKey:secret]) should] beTrue];
     });
     
     it(@"should fail to verify JWT with invalid secret", ^{
-        NSString *secret = @"notTheSecret";
-        NSString *signingInput = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
-        
-        [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKey:secret]) should] beFalse];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:signature verificationKey:wrongSecret]) should] beFalse];
     });
     
     it(@"should fail to verify JWT with invalid signature", ^{
-        NSString *secret = @"secret";
-        NSString *signingInput = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *signature = nil;
-        
-        [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKey:secret]) should] beFalse];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:nil verificationKey:secret]) should] beFalse];
     });
     
     it(@"should verify JWT with valid signature and secret Data", ^{
-        NSData *secretData = [NSData dataWithBase64String:[@"secret" base64String]];
-        NSString *signingInput = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
-        
-        [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKeyData:secretData]) should] beTrue];
+        __auto_type secretData = [NSData dataWithBase64String:[secret base64String]];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:signature verificationKeyData:secretData]) should] beTrue];
     });
     
     it(@"should fail to verify JWT with invalid secret", ^{
-        NSData *secretData = [NSData dataWithBase64String:[@"notTheSecret" base64String]];
-        NSString *signingInput = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
-        
-        [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKeyData:secretData]) should] beFalse];
+        __auto_type secretData = [NSData dataWithBase64String:[wrongSecret base64String]];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:signature verificationKeyData:secretData]) should] beFalse];
     });
     
     it(@"should fail to verify JWT with invalid signature", ^{
-        NSData *secretData = [NSData dataWithBase64String:[@"secret" base64String]];
-        NSString *signingInput = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
-        NSString *signature = nil;
-        
-        [[theValue([algorithm verifySignedInput:signingInput withSignature:signature verificationKeyData:secretData]) should] beFalse];
+        __auto_type secretData = [NSData dataWithBase64String:[secret base64String]];
+        [[theValue([algorithm verifySignedInput:signedPayload withSignature:nil verificationKeyData:secretData]) should] beFalse];
     });
 });
 
---
+++ Tests/Tests/Algorithms/JWTAlgorithmHSTests.m
@@ -0,0 +1,148 @@
+//
+//  JWTAlgorithmHSTests.m
+//  iOS_Tests
+//
+//  Created by Dmitry on 7/29/18.
+//
+
+#import <XCTest/XCTest.h>
+#import <Base64/MF_Base64Additions.h>
+#import <JWT/JWT.h>
+
+@interface JWTAlgorithmHSTestsHelper : NSObject
+@property (copy, nonatomic, readwrite) NSString *payload;
+@property (copy, nonatomic, readwrite) NSString *signedPayload;
+@property (copy, nonatomic, readwrite) NSString *signedPayloadBase64;
+@property (copy, nonatomic, readwrite) NSString *secret;
+@property (copy, nonatomic, readwrite) NSString *wrongSecret;
+@property (copy, nonatomic, readwrite) NSString *signature;
+@property (copy, nonatomic, readwrite) NSString *name;
+- (instancetype)configuredByName:(NSString *)name;
+
+@property (strong, nonatomic, readwrite) id <JWTAlgorithm> algorithm;
+@end
+
+@implementation JWTAlgorithmHSTestsHelper
+- (NSDictionary <NSString *, JWTAlgorithmHSTestsHelper *(^)(void)>*)configurations {
+    __weak __auto_type weakSelf = self;
+    return @{
+             JWTAlgorithmNameHS256 : ^{
+                 weakSelf.signedPayload = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+                 weakSelf.signedPayloadBase64 = @"uC/LeRrOxXhZuYm0MKgmSIzi5Hn9+SMmvQoug3WkK6Q=";
+                 weakSelf.signature = @"TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
+             },
+
+             JWTAlgorithmNameHS384 : ^{
+                 weakSelf.signedPayload = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+                 weakSelf.signedPayloadBase64 = @"s62aZf5ZLMSvjtBQpY4kiJbYxSu8wLAUop2D9nod5Eqgd+nyUCEj+iaDuVuI4gaJ";
+                 weakSelf.signature = @"hnzqaUFa2kfSFnynQ_WBJ7-wpLCgsyEdilCkRKliadjVuG-hGnc1qhvIjlvxSie5";
+             },
+             JWTAlgorithmNameHS512 : ^{
+                 weakSelf.signedPayload = @"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9";
+                 weakSelf.signedPayloadBase64 = @"KR3aqiPK+jqq4cl1U5H0vvNbvby5JzmlYYpciW9lINKw0o0tKYfayXR54xIUpR2Wz86voo5GpPlhtjxGNSoYng==";
+                 weakSelf.signature = @"SerC5MWQIs3fRH6ZD7gKKbq51TsyydXTvl23WpD9sA085SzQ7pK6M0TnYjFITNUkwuniGG5Is2OKJCEIHPn1Kg";
+             }
+             };
+}
+- (void)generalConfigure {
+    self.payload = @"payload";
+    self.secret = @"secret";
+    self.wrongSecret = @"notTheSecret";
+}
+- (instancetype)configuredByName:(NSString *)name {
+    __auto_type configured = [self configurations][name];
+    if (configured) {
+        [self generalConfigure];
+        configured();
+        self.name = name;
+    }
+    // choose configuration here?
+    self.algorithm = [JWTAlgorithmFactory algorithmByName:name];
+    return self;
+}
+@end
+
+@interface JWTAlgorithmHSTests : XCTestCase
+
+@end
+
+@implementation JWTAlgorithmHSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testAlgorithms {
+    {
+        [self theTestForAlgorithmName:@"HS256"];
+    }
+    {
+        [self theTestForAlgorithmName:@"HS384"];
+    }
+    {
+        [self theTestForAlgorithmName:@"HS512"];
+    }
+}
+
+- (void)theTestForAlgorithmName:(NSString *)name {
+    __block __auto_type helper = [[JWTAlgorithmHSTestsHelper new] configuredByName:name];
+
+    XCTAssertNotNil(helper);
+    XCTAssertNotNil(helper.algorithm);
+
+    [XCTContext runActivityNamed:@"name is from HS family" block:^(id<XCTActivity>  _Nonnull activity) {
+        XCTAssertEqualObjects(helper.algorithm.name, helper.name);
+    }];
+    
+    [XCTContext runActivityNamed:@"HMAC encodes the payload using SHA256" block:^(id<XCTActivity> _Nonnull activity) {
+        __auto_type encodedPayload = [helper.algorithm encodePayload:helper.payload withSecret:helper.secret];
+        XCTAssertEqualObjects([encodedPayload base64String], helper.signedPayloadBase64);
+    }];
+    
+    [XCTContext runActivityNamed:@"HMAC encodes the payload data using SHA256" block:^(id<XCTActivity> _Nonnull activity) {
+        __auto_type payloadData = [NSData dataWithBase64String:[helper.payload base64String]];
+        __auto_type secretData = [NSData dataWithBase64String:[helper.secret base64String]];
+        
+        __auto_type encodedPayload = [helper.algorithm encodePayloadData:payloadData withSecret:secretData];
+        XCTAssertEqualObjects([encodedPayload base64String], helper.signedPayloadBase64);
+    }];
+    
+    [XCTContext runActivityNamed:@"HMAC encodes the payload canonically" block:^(id<XCTActivity> _Nonnull activity) {
+        XCTAssertTrue([helper.algorithm verifySignedInput:helper.signedPayload withSignature:helper.signature verificationKey:helper.secret]);
+    }];
+
+    [XCTContext runActivityNamed:@"should verify JWT with valid signature and secret" block:^(id<XCTActivity> _Nonnull activity) {
+        XCTAssertTrue([helper.algorithm verifySignedInput:helper.signedPayload withSignature:helper.signature verificationKey:helper.secret]);
+    }];
+
+    [XCTContext runActivityNamed:@"should fail to verify JWT with invalid secret" block:^(id<XCTActivity> _Nonnull activity) {
+        XCTAssertFalse([helper.algorithm verifySignedInput:helper.signedPayload withSignature:helper.signature verificationKey:helper.wrongSecret]);
+    }];
+
+    [XCTContext runActivityNamed:@"should fail to verify JWT with invalid signature" block:^(id<XCTActivity> _Nonnull activity) {
+        XCTAssertFalse([helper.algorithm verifySignedInput:helper.signedPayload withSignature:nil verificationKey:helper.secret]);
+    }];
+
+    [XCTContext runActivityNamed:@"should verify JWT with valid signature and secret Data" block:^(id<XCTActivity> _Nonnull activity) {
+        __auto_type secretData = [NSData dataWithBase64String:[helper.secret base64String]];
+        XCTAssertTrue([helper.algorithm verifySignedInput:helper.signedPayload withSignature:helper.signature verificationKeyData:secretData]);
+    }];
+
+    [XCTContext runActivityNamed:@"should fail to verify JWT with invalid secret" block:^(id<XCTActivity> _Nonnull activity) {
+        __auto_type secretData = [NSData dataWithBase64String:[helper.wrongSecret base64String]];
+        XCTAssertFalse([helper.algorithm verifySignedInput:helper.signedPayload withSignature:helper.signature verificationKeyData:secretData]);
+    }];
+
+    [XCTContext runActivityNamed:@"should fail to verify JWT with invalid signature" block:^(id<XCTActivity> _Nonnull activity) {
+        __auto_type secretData = [NSData dataWithBase64String:[helper.secret base64String]];
+        XCTAssertFalse([helper.algorithm verifySignedInput:helper.signedPayload withSignature:nil verificationKeyData:secretData]);
+    }];
+}
+
+@end
