ee395dc092af70048b41f23560c3a211562b972d
Dmitry Lobanov <gaussblurinc@gmail.com>
2018-07-31 23:38:39 +0300

    scripts: generate keys script has been updated. various bugs have been fixed.

--- Scripts/generate_ec_curved_keys.rb
+++ Scripts/generate_ec_curved_keys.rb
@@ -86,9 +86,44 @@ class KeysGenerator
 			self.class.to_pem(self.name)
 		end
 	end
-	attr_accessor :curve_name, :secret
+
+	class << self
+		def available_ec_curves
+			#  secp256r1 - alias?
+			%w(prime256v1 secp384r1 secp512r1)
+		end
+		def available_rsa_sizes
+			[ 2048, 3072, 4096 ]
+		end
+	end
+
+
+	attr_accessor :type, :curve_name, :rsa_size, :secret
 	def initialize(secret = 'secret')
 		self.secret = secret
+	end
+
+	TYPES = [:ec, :rsa]
+	def type=(value)
+		@type = value.downcase.to_sym
+	end
+
+	def valid?
+		self.class::TYPES.include?(self.type)
+	end
+
+	def fix
+		@curve_name ||= self.class.available_ec_curves.first if self.type == :ec
+		@rsa_size ||= self.class.available_rsa_sizes.first if self.type == :rsa
+	end
+
+	def to_s
+		{
+			key_type: self.type,
+			curve_name: self.curve_name,
+			rsa_size: self.rsa_size,
+			rsa_private_key_passphrase: self.secret
+		}
 	end
 
 	module Generating
@@ -124,9 +159,31 @@ class KeysGenerator
 		def suppress_prompt(command)
 			%Q(echo '#{secret}' | #{command})
 		end
-		def generate_key(curve_name, name)
-			%Q(#{tool} ecparam -name #{curve_name} -genkey -noout -out #{name})
+
+		def generate_key(type, name, parameters)
+			case type
+			when :rsa
+				size = parameters[:size]
+				rsa_private_key_passphrase = parameters[:rsa_private_key_passphrase]
+				#%Q(#{tool} genrsa -des3 -out #{name} #{size} -passin 'password')
+				#%Q(#{tool} genrsa -aes256 -passin pass:#{rsa_private_key_passphrase} -out #{name} #{size})
+				# ssh-keygen -t rsa -b 4096 -f jwtRS256.key -q -N secret_phrase ( -N - NewPassphrase )
+				# -q -N #{rsa_private_key_passphrase}
+				%Q(echo "y" | ssh-keygen -t #{type} -b #{size} -f #{name} -q -N '')
+			when :ec
+				curve_name = parameters[:curve_name]
+				%Q(#{tool} ecparam -name #{curve_name} -genkey -noout -out #{name})
+			end
 		end
+
+		# def generate_key_rsa(parameter, name)
+		# 	%Q(#{tool} genrsa -des3 -out #{name} #{size} -passin 'password')
+		# 	# %Q(#{tool} genrsa -name #{parameter} -genkey -noout -out #{name})
+		# end
+
+		# def generate_key_ec(curve_name, name)
+		# 	%Q(#{tool} ecparam -name #{curve_name} -genkey -noout -out #{name})
+		# end
 
 		# output keys
 		def output_key(access, generated_key_name, name)
@@ -139,8 +196,8 @@ class KeysGenerator
 			output_key('private', generated_key_name, name)
 		end
 
-		def export_public_key(private_key_pem, public_key_pem)
-			%Q(#{tool} ec -in #{private_key_pem} -pubout -out #{public_key_pem})
+		def export_public_key(type, private_key_pem, public_key_pem)
+			%Q(#{tool} #{type} -in #{private_key_pem} -pubout -out #{public_key_pem})
 			#openssl ec -in ec256-private.pem -pubout -out ec256-public.pem
 		end
 
@@ -165,7 +222,7 @@ class KeysGenerator
 			%Q(#{tool} pkcs12 -export -in #{certificate} -inkey #{private_key_pem} -out #{p12_name} #{the_password_key_value})
 		end
 		def output_password(file, password = 'password')
-			%Q(echo "#{password}" > #{file})
+			%Q(echo -n "#{password}" > #{file})
 		end
 	end
 
@@ -173,23 +230,35 @@ class KeysGenerator
 		include Generating
 	end
 
-	def generated_by_curve
-		# curve name only?
-		curve_name = self.curve_name
-		generated(curve_name, "#{curve_name}-private", "#{curve_name}-public", "#{curve_name}", "#{curve_name}", "#{curve_name}", "#{curve_name}")
+	def generated_by_type(type = self.type)
+		# we need name and parameters?
+		parameters = {}
+		name = nil
+		case self.type
+		when :rsa
+			parameters[:size] = self.rsa_size
+			parameters[:rsa_private_key_passphrase] = self.secret
+			name = "rsa-#{self.rsa_size}"
+		when :ec
+			parameters[:curve_name] = self.curve_name
+			name = "ec-#{self.curve_name}"
+		end
+
+		self.generated(type, parameters, "#{name}-private", "#{name}-public", "#{name}", "#{name}", "#{name}-private", "#{name}-p12-password")
 	end
 
-	def generated(curve_name, private_key_pem, public_key_pem, certificate_request, certificate, p12, p12_password)
+	def generated(type, parameters, private_key_pem, public_key_pem, certificate_request, certificate, p12, p12_password)
 		the_private_key_pem ||= FileFormats.to_pem private_key_pem
 		the_public_key_pem ||= FileFormats.to_pem public_key_pem
 		the_certificate_request ||= FileFormats.to_certificate_request certificate_request
-		the_certificate ||= FileFormats.to_certificate certificate, '.pem'
+		the_certificate ||= FileFormats.to_certificate certificate
 		the_p12 ||= FileFormats.to_p12 p12
 		the_p12_password ||= FileFormats.to_plain_text p12_password
 
 		[
-			self.class.generate_key(curve_name, the_private_key_pem),
-			self.class.export_public_key(the_private_key_pem, the_public_key_pem),
+			self.class.generate_key(type, the_private_key_pem, parameters),
+			#self.class.generate_key(curve_name, the_private_key_pem),
+			self.class.export_public_key(type, the_private_key_pem, the_public_key_pem),
 			self.class.output_certificate_request(the_private_key_pem, the_certificate_request, self.class.default_subject),
 			self.class.output_certificate(the_certificate_request, the_private_key_pem, the_certificate),
 			self.class.output_password(the_p12_password),
@@ -215,6 +284,12 @@ class MainWork
 		end
 		options
 	end
+	def required_keys
+		[:key_type]
+	end
+	def valid_options?(the_options)
+		(required_keys - the_options.keys).empty?
+	end
 	def work(options = {})
 		options = fix_options(options)
 
@@ -222,12 +297,30 @@ class MainWork
 			puts "options are: #{options}"
 		end
 
+		unless valid_options? options
+			puts "options are not valid!"
+			puts "options are: #{options}"
+			puts "missing options: #{required_keys}"
+			exit(0)
+		end
+
 		ShellExecutor.setup options[:dry_run]
 
 		generator = KeysGenerator.new
+		generator.type = options[:key_type]
 		generator.curve_name = options[:curve_name]
+		generator.rsa_size = options[:rsa_size]
 
-		generator.generated_by_curve.each do |command|
+		generator.fix
+
+		puts "generator fixed arguments: #{generator.to_s}"
+
+		unless generator.valid?
+			puts "generator types are: #{KeysGenerator::TYPES}. You type is: #{generator.type}"
+			exit(0)
+		end
+
+		generator.generated_by_type(generator.type).each do |command|
 			ShellExecutor.run_command_line command
 		end
 		 #KeyParameters.new(options[:algorithm_type], options[:key_length])
@@ -270,6 +363,8 @@ class MainWork
 			opts.on('-o', '--output_directory DIRECTORY', 'Output Directory') {|v| options[:output_directory] = v}
 			opts.on('-t', '--test', 'Test option') {|v| options[:test] = v}
 			opts.on('-c', '--curve_name CURVE_NAME', 'Curve name') {|v| options[:curve_name] = v}
+			opts.on('-s', '--size SIZE', 'RSA key size') {|v| options[:rsa_size] = v}
+			opts.on('-k', '--key_type KEY_TYPE', 'Key Type (RSA or EC), case insensitive') {|v| options[:key_type] = v}
 			opts.on('-g', '--generated_key_name NAME', 'Generated key name') {|v| options[:generated_key_name] = v}
 			opts.on('-r', '--private_key_name NAME', 'Private Key Name') {|v| options[:private_key_name] = v}
 			opts.on('-u', '--public_key_name NAME', 'Public Key Name') {|v| options[:public_key_name] = v}
