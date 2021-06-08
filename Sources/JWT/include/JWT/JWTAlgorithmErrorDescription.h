495dd633285ae598d6f2d7373dc0df04ce7916c6
Dmitry Lobanov <gaussblurinc@gmail.com>
2021-06-08 14:54:50 +0300

    sources: jwt algorithm error description unnecessary todo has been removed.

--- Sources/JWT/include/JWT/JWTAlgorithmErrorDescription.h
+++ Sources/JWT/include/JWT/JWTAlgorithmErrorDescription.h
@@ -8,8 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-// TODO: Discuss
-// Rename to error or not necessary?
 @interface JWTAlgorithmErrorDescription : NSObject
 + (NSError *)errorWithCode:(NSInteger)code;
 + (NSError *)errorWithExternalError:(NSError *)error;
