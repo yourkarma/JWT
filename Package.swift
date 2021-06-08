1c9591dace2d64922603ba39fc2f07690517209b
Dmitry Lobanov <gaussblurinc@gmail.com>
2021-06-08 13:22:50 +0300

    distribution: base64 dependency has been added.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -17,11 +17,12 @@ let package = Package(
             targets: ["JWT"]
         )
     ],
-    dependencies: [],
+    dependencies: [.package(url: "https://github.com/lolgear/Base64", ._branchItem("distribution/swift_package_manager_support"))],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "JWT",
+                dependencies: ["Base64"],
                 exclude: ["FrameworkSupplement"])
     ]
 )
